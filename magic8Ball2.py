713502dc27c4bc7d69cf7ed82c3ccc5588bf8f6b
dv200103 <134840696+dv200103@users.noreply.github.com>
2023-10-04 19:47:29 +0530

    Add files via upload
    In this program we understand that len() function always start counting from 1, and indexes starts from the 0 that's why in program it is written that print(messages[random.randint(0,len(messages)-1)]), so 9-1 is 8 and there is 8 messages according to indexes(0 to 8).

---
+++ magic8Ball2.py
@@ -0,0 +1,5 @@
+import random
+
+messages = ['It is certain','It is decidely so','Yes definetly','Reply hazy try again','Ask again later','Concentrate ask again','My reply is no','Outlook not so good','Very doubtful']
+
+print(messages[random.randint(0,len(messages)-1)])
